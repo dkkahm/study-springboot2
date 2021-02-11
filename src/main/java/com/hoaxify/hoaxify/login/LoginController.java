e6a00eab6d9b8f942b4c4beb2af8bb1700802ec4
ndkkahm <ndkkahm@gmail.com>
2021-02-11 23:16:01 +0900

    5.Login 5. Authenticated User (Backend)

--- src/main/java/com/hoaxify/hoaxify/login/LoginController.java
+++ src/main/java/com/hoaxify/hoaxify/login/LoginController.java
@@ -1,17 +1,22 @@
 package com.hoaxify.hoaxify.login;
 
 import com.hoaxify.hoaxify.error.ApiError;
+import com.hoaxify.hoaxify.shared.CurrentUser;
+import com.hoaxify.hoaxify.user.User;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.AccessDeniedException;
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/1.0/login")
 public class LoginController {
 
-    @PostMapping("")
-    void handleLogin() {
-
+    @PostMapping("/api/1.0/login")
+    Map<String, Object> handleLogin(@CurrentUser User loggedInUser) {
+        return Collections.singletonMap("id", loggedInUser.getId());
     }
 }
