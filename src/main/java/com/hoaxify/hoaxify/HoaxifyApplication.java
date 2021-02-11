404c6cec729f2728c7699cfdbd8f8420feddb400
ndkkahm <ndkkahm@gmail.com>
2021-02-12 08:21:52 +0900

    8. Listing Users 8. Creating Random Users (Backend)

--- src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
+++ src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
@@ -1,8 +1,15 @@
 package com.hoaxify.hoaxify;
 
+import com.hoaxify.hoaxify.user.User;
+import com.hoaxify.hoaxify.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class HoaxifyApplication {
@@ -11,4 +18,19 @@ public class HoaxifyApplication {
 		SpringApplication.run(HoaxifyApplication.class, args);
 	}
 
+	@Bean
+	@Profile("!test")
+	CommandLineRunner run(UserService userService) {
+		return (args) -> {
+			IntStream.rangeClosed(1, 15)
+				.mapToObj(i -> {
+					User user = new User();
+					user.setUsername("user" + i);
+					user.setDisplayName("display" + i);
+					user.setPassword("P4ssword");
+					return user;
+				})
+				.forEach(userService::save);
+		};
+	}
 }
