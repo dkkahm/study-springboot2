626a9bf4a7edea58f83062c7d5ed75aeced29d1f
ndkkahm <ndkkahm@gmail.com>
2021-02-12 13:12:11 +0900

    11. User Update Profile Image 4. External Configuration (Backend)

--- src/test/java/com/hoaxify/hoaxify/etc/StaticResourceTest.java
+++ src/test/java/com/hoaxify/hoaxify/etc/StaticResourceTest.java
@@ -1,7 +1,10 @@
 package com.hoaxify.hoaxify.etc;
 
+import com.hoaxify.hoaxify.configuration.AppConfiguration;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -14,11 +17,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class StaticResourceTest {
+    @Autowired
+    AppConfiguration appConfiguration;
 
     @Test
     public void checkStaticFolder_whenApplicationIsInitialized_uploadFolderMustExist() {
-        File uploadFolder = new File("uploads-test");
+        File uploadFolder = new File(appConfiguration.getUploadPath());
         boolean uploadFolderExists = uploadFolder.exists() && uploadFolder.isDirectory();
         assertThat(uploadFolderExists).isTrue();
+    }
+
+    @Test
+    public void checkStaticFolder_whenApplicationIsInitialized_profileImageSubFolderMustExist() {
+        String profileImageFolderPath = appConfiguration.getFullProfileImagesPath();
+        File profileImageFolder = new File(profileImageFolderPath);
+        boolean profileImageFolderExist = profileImageFolder.exists() && profileImageFolder.isDirectory();
+        assertThat(profileImageFolderExist).isTrue();
+    }
+
+    @Test
+    public void checkStaticFolder_whenApplicationIsInitialized_attachmentSubFolderMustExist() {
+        String attachmentFolderPath = appConfiguration.getFullAttachmentPath();
+        File attachmentFolder = new File(attachmentFolderPath);
+        boolean attachmentFolderExist = attachmentFolder.exists() && attachmentFolder.isDirectory();
+        assertThat(attachmentFolderExist).isTrue();
     }
 }
